33d4a02223c30f92d3e74c96fe8b0b3572f88185
Lenoch <81lennoch@gmail.com>
2023-11-18 15:33:34 +0100

    Update start.py

--- start.py
+++ start.py
@@ -8,14 +8,11 @@ def run_spawn_all():
     subprocess.run(['python', 'spawn-all.py'])
 
 if __name__ == "__main__":
-    # Créer les threads pour chaque programme
     server_thread = threading.Thread(target=run_server)
     spawn_all_thread = threading.Thread(target=run_spawn_all)
 
-    # Démarrer les threads
     server_thread.start()
     spawn_all_thread.start()
 
-    # Attendre que les threads se terminent
     server_thread.join()
     spawn_all_thread.join()
